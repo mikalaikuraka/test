f55dcd10f4af9fb22d484862d23d4df9d7136af1
Mikalai Kuraka <kurakokolia@gmail.com>
2022-11-16 20:11:29 +0300

    add func check

--- test.py
+++ test.py
@@ -2,41 +2,52 @@ import requests
 import json
 
 
-log = {}
-
-def connection_check(url):
-
-    url = url.strip()
-
-    responce_get = requests.get(url)
-    code_get = responce_get.status_code
-
-    responce_post = requests.post(url)
-    code_post = responce_post.status_code
 
 
-    if code_get != 405:
-        mtd = 'GET'
-        opt = requests.options(url)
-        answer = {'OPTIONS' : opt.status_code}
-        status_code = {mtd : code_get}
-        answer.update(status_code)
-        status = {url : answer}
-        log.update(status)
-    else:
-        pass
+def check(urls):
 
-    if code_post != 405:
-        mtd = 'POST'
-        opt = requests.options(url)
-        answer = {'OPTIONS' : opt.status_code}
-        status_code = {mtd : code_post}
-        answer.update(status_code)
-        status = {url : answer}
-        log.update(status)
-    else:
-        pass
+    log = {}
 
+    def connection_check(url):
+
+        url = url.strip()
+
+        responce_get = requests.get(url)
+        code_get = responce_get.status_code
+
+        responce_post = requests.post(url)
+        code_post = responce_post.status_code
+
+
+        if code_get != 405:
+            mtd = 'GET'
+            opt = requests.options(url)
+            answer = {'OPTIONS' : opt.status_code}
+            status_code = {mtd : code_get}
+            answer.update(status_code)
+            status = {url : answer}
+            log.update(status)
+        else:
+            pass
+
+        if code_post != 405:
+            mtd = 'POST'
+            opt = requests.options(url)
+            answer = {'OPTIONS' : opt.status_code}
+            status_code = {mtd : code_post}
+            answer.update(status_code)
+            status = {url : answer}
+            log.update(status)
+        else:
+            pass
+
+    for i in range(len(urls)):
+        connection_check(urls[i])
+
+    with open('file.json', 'w') as f:
+        json.dump(log, f)
+
+    print(log)
 
 urls = [
     'https://en.wikipedia.org/w/index.php',
@@ -46,11 +57,4 @@ urls = [
     'https://habr.com/ru/post/593529/', 'https://pythonworld.ru/tipy-dannyx-v-python/fajly-rabota-s-fajlami.html'
     ]
 
-
-for i in range(len(urls)):
-    connection_check(urls[i])
-
-with open('file.json', 'w') as f:
-    json.dump(log, f)
-
-print(log)
+check(urls)
