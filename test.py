1ee33b121d1116fbfd8a7536064f5b712848ccf0
Mikalai Kuraka <kurakokolia@gmail.com>
2022-11-16 18:56:39 +0300

    add random urls

--- test.py
+++ test.py
@@ -1,1 +1,11 @@
+import requests
+import json
+
+urls = [
+    'https://en.wikipedia.org/w/index.php',
+    'http://www.google.com',
+    'https://pythonworld.ru/tipy-dannyx-v-python/slovari-dict-funkcii-i-metody-slovarej.html', 
+    'https://ru.hexlet.io/courses/python-setup-environment/lessons/start-with-poetry/theory_unit',
+    'https://habr.com/ru/post/593529/', 'https://pythonworld.ru/tipy-dannyx-v-python/fajly-rabota-s-fajlami.html'
+    ]
 
