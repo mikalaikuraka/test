2a31e465b903cf7c74434e21434dcb2bbeb3d2c9
Mikalai Kuraka <kurakokolia@gmail.com>
2022-11-16 19:12:54 +0300

    add cycle

--- test.py
+++ test.py
@@ -1,13 +1,6 @@
 import requests
 import json
 
-urls = [
-    'https://en.wikipedia.org/w/index.php',
-    'http://www.google.com',
-    'https://pythonworld.ru/tipy-dannyx-v-python/slovari-dict-funkcii-i-metody-slovarej.html', 
-    'https://ru.hexlet.io/courses/python-setup-environment/lessons/start-with-poetry/theory_unit',
-    'https://habr.com/ru/post/593529/', 'https://pythonworld.ru/tipy-dannyx-v-python/fajly-rabota-s-fajlami.html'
-    ]
 
 log = {}
 
@@ -45,4 +38,15 @@ def connection_check(url):
         pass
 
 
+urls = [
+    'https://en.wikipedia.org/w/index.php',
+    'http://www.google.com',
+    'https://pythonworld.ru/tipy-dannyx-v-python/slovari-dict-funkcii-i-metody-slovarej.html', 
+    'https://ru.hexlet.io/courses/python-setup-environment/lessons/start-with-poetry/theory_unit',
+    'https://habr.com/ru/post/593529/', 'https://pythonworld.ru/tipy-dannyx-v-python/fajly-rabota-s-fajlami.html'
+    ]
+
+
+for i in range(len(urls)):
+    connection_check(urls[i])
 
