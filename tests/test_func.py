b4ed2419e0b5f9411e5cb7b4fbb878288ea569dd
Mikalai Kuraka <kurakokolia@gmail.com>
2022-11-19 21:17:01 +0300

    add passed tests

--- tests/test_func.py
+++ tests/test_func.py
@@ -1,15 +1,18 @@
-from test import check, connection_check, requests1, urls
+from ..test import check, connection_check, requests1, urls, log
 
 """TESTING FUNCTION"""
 
-# def test_check():
-#     assert check(3) == 5
-#     pass
+def test_check():
+    assert check(urls) == None
 
-def test_requests1():
-    assert requests1(200, urls[1]) == None
+def test_requests_GET():
+    assert requests1(200, urls[1], "GET") == None
 
+def test_requests_POST():
+    assert requests1(200, urls[1], "POST") == None
 
-# def test_connection_check():
-#     assert connection_check(url) == 5
+def test_requests_GET_failed():
+    assert requests1(405, urls[1], "GET") == None
 
+def test_requests_POST_failed():
+    assert requests1(405, urls[1], "POST") == None
